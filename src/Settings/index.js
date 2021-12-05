1b7ade909f76507f3b0bbeb1cb918aa3091c67fa
Dhruv <dev.aroradhruv@gmail.com>
2021-12-05 22:39:58 +0530

    CoinGrid component added

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
-import ConfirmButton from './ConfirmButton';
-import Page from '../Shared/Page';
+import ConfirmButton from "./ConfirmButton";
+import Page from "../Shared/Page";
+import CoinGrid from "./CoinGrid";
 
-export default function(){
-    return(
-        <Page name = "settings">
-            <WelcomeMessage/>
-            <ConfirmButton/>
-        </Page>
-    )
+export default function () {
+  return (
+    <Page name="settings">
+      <WelcomeMessage />
+      <ConfirmButton />
+      <CoinGrid />
+    </Page>
+  );
 }
