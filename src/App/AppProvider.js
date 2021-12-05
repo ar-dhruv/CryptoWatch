0be8cbc332d59f653cf0ff23cbd266a87c81e42d
Dhruv <dev.aroradhruv@gmail.com>
2021-12-05 16:51:33 +0530

    Settings page & localStorage

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -6,9 +6,32 @@ export class AppProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: "settings",
+      page: "dashboard",
+      ...this.savedSettings(),
       setPage: this.setPage,
+      confirmFavourites: this.confirmFavourites,
     };
+  }
+
+  confirmFavourites = () => {
+    this.setState({
+      firstVisit: false,
+      page: "dashboard",
+    });
+    localStorage.setItem(
+      "cryptoWatch",
+      JSON.stringify({
+        test: "hello",
+      })
+    );
+  };
+
+  savedSettings() {
+    let CryptoWatchData = JSON.parse(localStorage.getItem("cryptoWatch"));
+    if (!CryptoWatchData) {
+      return { page: "settings", firstVisit: true };
+    }
+    return {};
   }
 
   setPage = (page) => this.setState({ page });
