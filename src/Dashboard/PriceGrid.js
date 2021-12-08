38a231d13121ad7f7d24e2480bf71e60c6109241
Dhruv <dev.aroradhruv@gmail.com>
2021-12-08 08:19:55 +0530

    Styling priceTile

--- src/Dashboard/PriceGrid.js
+++ src/Dashboard/PriceGrid.js
@@ -5,17 +5,18 @@ import PriceTile from "./PriceTile";
 
 const PriceGrid = styled.div`
   display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px;
+  margin-top: 40px;
 `;
-
-
 
 export default function () {
   return (
     <AppContext.Consumer>
       {({ prices }) => (
         <PriceGrid>
-          {prices.map((price) => (
-           <PriceTile price = {price}/>
+          {prices.map((price, index) => (
+            <PriceTile index={index} price={price} />
           ))}
         </PriceGrid>
       )}
--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import styled , {css} from 'styled-components'
-import { SelectableTile } from '../Shared/Tile'
+import React from "react";
+import styled, { css } from "styled-components";
+import { SelectableTile } from "../Shared/Tile";
+import { fontSize3 , fontSizeBig} from "../Shared/Styles";
+import { CoinHeaderGridStyled } from "../Settings/CoinHeaderGrid";
 
-export default function({price , index}){
-let sym = Object.keys(price)[0];
-let data = price[sym]['USD'];
+const JustifyRight = styled.div`
+  justify-self: right;
+`;
 
-    return (
-        <SelectableTile>{sym} {data.PRICE}</SelectableTile>
-    )
+const TickerPrice = styled.div`
+${fontSizeBig}
+`
+
+const numberFormat = (number) => {
+  return +(number + "").slice(0, 7);
+};
+
+const PriceTileStyled = styled(SelectableTile)`
+  ${(props) =>
+    props.compact &&
+    css`
+      ${fontSize3}
+    `}
+`;
+
+function PriceTile({ sym, data }) {
+  return (
+    <PriceTileStyled>
+      <CoinHeaderGridStyled>
+        <div> {sym} </div>
+        <JustifyRight> {numberFormat(data.CHANGEPCT24HOUR)} </JustifyRight>
+      </CoinHeaderGridStyled>
+      <TickerPrice>
+          {numberFormat(data.PRICE)}
+      </TickerPrice>
+    </PriceTileStyled>
+  );
+}
+
+export default function ({ price, index }) {
+  let sym = Object.keys(price)[0];
+  let data = price[sym]["USD"];
+
+  return <PriceTile sym={sym} data={data}></PriceTile>;
 }
